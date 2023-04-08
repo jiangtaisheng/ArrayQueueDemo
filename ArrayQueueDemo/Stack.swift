0dd7ba9b8d287891885404cf0293ef3a551878bb
蒋太生 <949431495@qq.com>
2023-04-08 16:43:11 +0800

    feat:封装了栈stack

--- ArrayQueueDemo/Stack.swift
+++ ArrayQueueDemo/Stack.swift
@@ -11,6 +11,39 @@
 
 import UIKit
 
-class Stack: NSObject {
+class Stack {
 
+    fileprivate var array: [Any] = [] // 数据存储容器
+    var  isEmpty: Bool { // 判断是否是空栈
+        return array.count == 0
+    }
+    var  count: Int { // 查看栈的有效元素
+        return array.count
+    }
+    var  peek: Any? { // 查看栈的顶层元素
+        if array.count > 0 {
+            return array.last
+        }
+       return nil
+    }
+    
+    //入栈
+    func push(_ element: Any) {
+       array.append(element)
+    }
+    // 出栈
+    func pop() -> Any? {
+        return array.popLast()
+    }
+    
+    // 删除数组最后一个元素 并返回这个元素
+    private  func popLast() -> Any? {
+        if array.count > 0 {
+            let lastObj = array.last
+            array.remove(at: array.count-1)
+            return lastObj
+        }
+        return nil
+    }
+    
 }
--- ArrayQueueDemo/ViewController.swift
+++ ArrayQueueDemo/ViewController.swift
@@ -14,41 +14,70 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
 //        arrayQueueTest()
 //        arrayCycleQueueTest()
-        singleLinkedListTest()
+//        singleLinkedListTest()
+          stackTest()
     }
     
-    
-    func singleLinkedListTest(){
+    func stackTest(){
         
-        let node1: Node = Node.init(item: ["id":1,"name": "秦始皇", "country": "大秦帝国"])
-        let node2: Node = Node.init(item: ["id":2,"name": "汉武大帝", "country": "大汉帝国"])
-        let node3: Node = Node.init(item: ["id":3,"name": "隋炀帝", "country": "大隋帝国"])
-        let node4: Node = Node.init(item: ["id":4,"name": "唐太宗", "country": "大唐帝国"])
-        let node33: Node = Node.init(item: ["id":3,"name": "隋文帝", "country": "大隋帝国"])
+        let  data1 = ["name": "秦始皇", "country": "大秦帝国"]
+        let  data2 = ["name": "汉武大帝", "country": "大汉帝国"]
+        let  data3 = ["name": "隋炀帝", "country": "大隋帝国"]
+        let  data4 = ["name": "唐太宗", "country": "大唐帝国"]
+        let  data5 = ["name": "元世祖", "country": "大元帝国"]
+        
+        let stack =  Stack()
+        stack.push(data1)
+        stack.push(data2)
+        stack.push(data3)
+        stack.push(data4)
+        stack.push(data5)
+        print("stack-peek:")
+        print(stack.peek)
+        
+        print("stack-pop:")
 
-        let link: SingleLinkedList = SingleLinkedList()
-        
-        link.addSequenceNode(node: node1)
-        link.addSequenceNode(node: node4)
-        link.addSequenceNode(node: node2)
-        link.addSequenceNode(node: node3)
-        link.showList()
-//        print(SingleLinkedList.nodeLength(head: link.getHead()))
-//
-//        print("deleteNode:")
-//        link.deleteNode(node: node2)
-//        link.showList()
-//        print("updateNode:")
-//        link.updateNode(node: node33)
-//        link.showList()
-//
-//        print(SingleLinkHelp.nodeLength(head: link.getHead()))
-//        print(SingleLinkHelp.getNodeToInverseIndex(head: link.getHead(), inverseIndex: 0))
-        
-         print("inverseSingleLinkList:")
-         SingleLinkHelp.inverseSingleLinkList(head: link.getHead())
-         link.showList()
+        print(stack.pop())
+        print(stack.pop())
+        print(stack.pop())
+        print("stack-peek:")
+        print(stack.peek)
+        print("stack-count:")
+        print(stack.count)
+
     }
+    
+//    func singleLinkedListTest(){
+//
+//        let node1: Node = Node.init(item: ["id":1,"name": "秦始皇", "country": "大秦帝国"])
+//        let node2: Node = Node.init(item: ["id":2,"name": "汉武大帝", "country": "大汉帝国"])
+//        let node3: Node = Node.init(item: ["id":3,"name": "隋炀帝", "country": "大隋帝国"])
+//        let node4: Node = Node.init(item: ["id":4,"name": "唐太宗", "country": "大唐帝国"])
+//        let node33: Node = Node.init(item: ["id":3,"name": "隋文帝", "country": "大隋帝国"])
+//
+//        let link: SingleLinkedList = SingleLinkedList()
+//
+//        link.addSequenceNode(node: node1)
+//        link.addSequenceNode(node: node4)
+//        link.addSequenceNode(node: node2)
+//        link.addSequenceNode(node: node3)
+//        link.showList()
+////        print(SingleLinkedList.nodeLength(head: link.getHead()))
+////
+////        print("deleteNode:")
+////        link.deleteNode(node: node2)
+////        link.showList()
+////        print("updateNode:")
+////        link.updateNode(node: node33)
+////        link.showList()
+////
+////        print(SingleLinkHelp.nodeLength(head: link.getHead()))
+////        print(SingleLinkHelp.getNodeToInverseIndex(head: link.getHead(), inverseIndex: 0))
+//
+//         print("inverseSingleLinkList:")
+//         SingleLinkHelp.inverseSingleLinkList(head: link.getHead())
+//         link.showList()
+//    }
 
 //    func arrayQueueTest() {
 //
